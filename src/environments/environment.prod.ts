1633c5882a07a6c014e373b29c713ad5ecc8a4ef
NitinNair89 <nitin.dreamern89@gmail.com>
2020-05-18 22:23:10 +0530

    Added category URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,9 @@ export const environment = {
   production: true,
   apiURL: {
     "mealCategories": "https://www.themealdb.com/api/json/v1/1/list.php?c=list",
-    "randomMeal": "https://www.themealdb.com/api/json/v1/1/random.php"
+    "randomMeal": "https://www.themealdb.com/api/json/v1/1/random.php",
+    "allCategories": "https://www.themealdb.com/api/json/v1/1/categories.php",
+    "categoryInfo": "https://www.themealdb.com/api/json/v1/1/filter.php",
+    "mealsByCategory": "https://www.themealdb.com/api/json/v1/1/filter.php"
   }
 };
