d01744dd84a85203a544eb87eef74db12d51f5fc
NitinNair89 <nitin.dreamern89@gmail.com>
2020-05-18 15:48:54 +0530

    Removed JSON file, managed API URLs in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: {
+    "mealCategories": "https://www.themealdb.com/api/json/v1/1/list.php?c=list",
+    "randomMeal": "https://www.themealdb.com/api/json/v1/1/random.php"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: false
+  production: false,
+  apiURL: {
+    "mealCategories": "https://www.themealdb.com/api/json/v1/1/list.php?c=list",
+    "randomMeal": "https://www.themealdb.com/api/json/v1/1/random.php"
+  }
 };
