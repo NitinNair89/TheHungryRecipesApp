e3327d5f3d7f582ba75f12cab98193d04d7206f9
NitinNair89 <nitin.dreamern89@gmail.com>
2020-05-18 19:05:25 +0530

    [ABOUT]: Implemented route-level code-splitting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
   },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
   { path: 'random', loadChildren: () => import('./components/randomrecipe/randomrecipe.module').then(m => m.RandomrecipeModule) },
   { path: 'landing', loadChildren: () => import('./components/landing/landing.module').then(m => m.LandingModule) },
-  { path: '', loadChildren: () => import('./components/landing/landing.module').then(m => m.LandingModule) }
+  { path: '', loadChildren: () => import('./components/landing/landing.module').then(m => m.LandingModule) },
+  { path: 'about', loadChildren: () => import('./components/about/about.module').then(m => m.AboutModule) }
 ];
 
 @NgModule({
