a22b15b2ef4129af27350a94b4963c96a80f6763
4ist <frichardson062@gmail.com>
2020-10-08 16:03:59 -0400

    added express routes

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/getUserFavorites/:userID', (req, res) => {
+  res.send('-retrieved user favorites from db-')
+  // TODO mongoose call
+})
+
+app.post('/addUserFavorite/:recipeID', (req, res) => {
+  res.send('-added user favorite to db-')
+  //TODO mongoose call
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
