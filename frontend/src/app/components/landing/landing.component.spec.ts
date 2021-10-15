febb8a67ee8756fd06c545a30c4f21bcc04d533f
Koshima Goyal <32846043+koshimagoyal@users.noreply.github.com>
2021-10-15 23:39:48 +0530

    Testcase for Landing component
    Another testcase to test search functionality on landing component

--- frontend/src/app/components/landing/landing.component.spec.ts
+++ frontend/src/app/components/landing/landing.component.spec.ts
@@ -2,14 +2,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
   let fixture: ComponentFixture<LandingComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        RouterTestingModule.withRoutes([
+          {path: 'recipelist', loadChildren: () => import('../../components/recipelist/recipelist.module').then(m => m.RecipelistModule)}
+        ]),
         MatIconModule
       ],
       declarations: [ LandingComponent ]
@@ -20,6 +27,7 @@ describe('LandingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
@@ -41,4 +49,24 @@ describe('LandingComponent', () => {
     let heading = fixture.nativeElement.querySelector('h4');
     expect(heading.textContent).toEqual('Confused what to cook? Search a recipe or try a random recipe!');
   });
+  
+  it('should fail when search value is undefined', () => {
+    let inputValue = debugElement.query(By.css('input'));
+    component.recipeName = undefined;
+    inputValue.triggerEventHandler('keyup',component.navigate());
+    fixture.detectChanges();
+    let errorValue = debugElement.query(By.css('label')).nativeElement.textContent;
+    expect(errorValue).toEqual('Please provide a recipe name');
+  });
+
+  it('should navigate when search value is valid', () => {
+    let inputValue = debugElement.query(By.css('input'));
+    component.recipeName = 'BEEF';
+    inputValue.triggerEventHandler('keyup',component.navigate());
+    fixture.detectChanges();
+    let errorElement = debugElement.query(By.css('label'));
+    expect(errorElement).toBeNull();
+    expect(component.inputError).toEqual(false);
+  });
+  
 });
